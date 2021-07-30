5b5a9da1e56434869eda1655c717788d22faab13
syedhabeebuddin <syedhabeebuddin0@gmail.com>
2021-07-31 00:26:25 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import { of, from } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -11,11 +12,24 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     of(2, 4, 8, 9, 10).subscribe(console.log);
-    from([20, 15, 10, 5]).subscribe(
-      item => console.log(`item is : ${item}`),
-      err => console.error(`error occured ${err}`),
-      () => console.log('complete')
-    );
+    from([20, 15, 10, 5])
+      .pipe(
+        tap(item => console.log(`original item : ${item}`)),
+        map(item => item * 2),
+        map(item => item - 20),
+        map(item => {
+          if (item === 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+        take(3)
+      )
+      .subscribe(
+        item => console.log(`item is : ${item}`),
+        err => console.error(`error occured ${err}`),
+        () => console.log('complete')
+      );
 
     of('test1', 'test2', 'test3').subscribe(
       item => console.log(`Items are : ${item}`),
